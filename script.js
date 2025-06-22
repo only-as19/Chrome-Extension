ab4a7f97c7396da88309ff526599ad4e1dfc16f2
Asad <hellohello51981@gmail.com>
2025-06-22 23:23:58 +0500

    Create the render function

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const inputEl = document.getElementById("input-El")
 const ulEl = document.getElementById("ul-El");
 const deleteBtn = document.getElementById("deleteBtn");
 const tabBtn = document.getElementById("tab-btn");
+
+
+function render(leads){
+    let listItems = "";
+    for(let i = 0; i < leads.length; i++){
+        listItems +=`
+        <li>
+            <a target ='_blank' href ='${leads[i]}'>
+            ${leads[i]}
+            </a>
+        </li>`;
+        console.log(listItems)
+    }
+    ulEl.innerHTML = listItems;
+}
