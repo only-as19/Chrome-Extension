3a1a385c3bef21498419a80d8739b837cd1d488b
Asad <hellohello51981@gmail.com>
2025-06-22 23:22:58 +0500

    Add JS- Select all the buttons in variable for later and better use

---
+++ script.js
@@ -0,0 +1,6 @@
+let myLead = [];
+const savedBtn = document.getElementById("inputBtn");
+const inputEl = document.getElementById("input-El")
+const ulEl = document.getElementById("ul-El");
+const deleteBtn = document.getElementById("deleteBtn");
+const tabBtn = document.getElementById("tab-btn");
